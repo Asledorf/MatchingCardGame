9c5654fc4c0d749e8df6b0a7da6b0f2311214c72
Asledorf <KStover@student.neumont.edu>
2020-11-09 04:26:05 -0700

    Add project files.

---
+++ MatchingCardGame/Controls/Card.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchingCardGame.Controls
+{
+    /// <summary>
+    /// Interaction logic for Card.xaml
+    /// </summary>
+    public partial class Card : UserControl, INotifyPropertyChanged
+    {
+    //interface: INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged(string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+    //Base Class
+        //ctor
+        public Card()
+        {
+            InitializeComponent();
+            DataContext = this;
+            this.Loaded += Card_Loaded;
+        }
+
+        private void Card_Loaded(object sender, RoutedEventArgs e)
+        {
+            Owner = (GameWindow)Window.GetWindow(this);
+            Owner.RegisterCard(this);
+        }
+
+        public enum eState
+        {
+            INACTIVE,
+            IDLE,
+            FLIPPED,
+            MATCHED
+        }
+
+        public GameWindow Owner { get; set; }
+
+        private eState state = eState.INACTIVE;
+        public eState State 
+        { 
+            get { return state; }
+            set 
+            {
+                if (value != state)
+                {
+                    state = value;
+                    Interactable = (state == eState.IDLE);
+                    Show = (state == eState.FLIPPED || state == eState.MATCHED);
+                    NotifyPropertyChanged("State");
+                }
+            } 
+        }
+
+        private bool interactable = true;
+        public bool Interactable 
+        {
+            get { return interactable; }
+            set
+            {
+                //Console.WriteLine("Interactable for: \"" + this.Symbol + "\" has been set to " + value);
+                if (value != interactable)
+                {
+                    interactable = value;
+                    NotifyPropertyChanged("Interactable");
+                }
+            }
+        }
+        
+        private string symbol;
+        public string Symbol 
+        {
+            get { return symbol; }
+            set
+            {
+                if (value != symbol)
+                {
+                    symbol = value;
+                    NotifyPropertyChanged("Symbol");
+                }
+            }
+        }
+
+        public bool Show 
+        {
+            set 
+            {
+                if (value)
+                {
+                    lblSymbol.Visibility = Visibility.Visible;
+                    btnCard.Visibility = Visibility.Hidden;
+                }
+                else
+                {
+                    lblSymbol.Visibility = Visibility.Hidden;
+                    btnCard.Visibility = Visibility.Visible;
+                }
+            } 
+        }
+
+        //click event
+        private void btnCard_Click(object sender, RoutedEventArgs e)
+        {
+            State = eState.FLIPPED;
+            Owner.SelectCard(this);
+        }
+    }
+}
---
+++ MatchingCardGame/GameWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using MatchingCardGame.Controls;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace MatchingCardGame
+{
+    /// <summary>
+    /// Interaction logic for GameWindow.xaml
+    /// </summary>
+    public partial class GameWindow : Window
+    {
+        //ctor
+        public GameWindow()
+        {
+            InitializeComponent();
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(1);
+            dispatcherTimer.Tick += DispatcherTimer_Tick;
+        }
+
+        //members & props
+        private Random random = new Random();
+        private DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        private List<Card> cards = new List<Card>();
+        private List<string> symbols = new List<string>()
+        {
+            "A","A","B","B","C","C",
+            "D","D","E","E","F","F"
+        };
+        private Card card1 = null;
+        private Card card2 = null;
+
+        //methods
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            dispatcherTimer.Stop();
+            foreach (var c in cards)
+            {
+                if (c.State != Card.eState.MATCHED)
+                {
+                    c.State = Card.eState.IDLE;
+                }
+            }
+            card1 = null;
+            card2 = null;
+        }
+        public void RegisterCard(Card card)
+        {
+            card.State = Card.eState.IDLE;
+            int r = random.Next(symbols.Count);
+            card.Symbol = symbols[r];
+            symbols.RemoveAt(r);
+            cards.Add(card);
+        }
+        public void SelectCard(Card card)
+        {
+            if (card1 == null)
+            {
+                card1 = card;
+            }
+            else
+            {
+                card2 = card;
+                if (card1.Symbol == card2.Symbol)
+                {
+                    card1.State = Card.eState.MATCHED;
+                    card2.State = Card.eState.MATCHED;
+
+                    card1 = null;
+                    card2 = null;
+                }
+                else
+                {
+                    foreach (var c in cards)
+                    {
+                        if (card.State == Card.eState.IDLE)
+                        {
+                            card.State = Card.eState.INACTIVE;
+                        }
+                    }
+                    dispatcherTimer.Start();
+                }
+            }
+        }
+    }
+}
